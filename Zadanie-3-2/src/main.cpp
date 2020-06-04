9e76a2b84067ec35a4c62202d1abaca2f28d4574
Mateusz Jankowski <mateusz.jankowski@student.wat.edu.pl>
2020-06-04 13:20:35 +0200

    Wykonano zadanie 3.2.3 – T/C1 rejestry

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,19 +1,22 @@
-#include <Arduino.h>
-#define ADC_RES_DEF (5.0f / 1024.0f)
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-void setup() {
-  Serial.begin(9600);
-  analogReference(DEFAULT);
-  pinMode(A5, INPUT);
+int main(void) {
+  DDRD &= ~(1 << DDD5);
+  PORTD |= (1 << PORTD5);
+  TIMSK1 |= (1 << TOIE1);
+  TCCR1B |= (1 << CS12) | (1 << CS11) | (1 << CS10);
+  sei();
+
+  while (1)
+  {
+		{
+		if (TCNT1 > 256)
+		PORTC |= (1 << PORTC2);
+		}
+  }
 }
 
-void loop() {
-  uint16_t digital = analogRead(A5);
-  float voltage = ADC_RES_DEF * digital;
-  Serial.print("A5 = ");
-  Serial.print(digital);
-  Serial.print(" \t V(5) = ");
-  Serial.print(voltage);
-  Serial.println(" [V]");
-  delay(1000);
+ISR(TIMER1_OVF_vect) {
+  
 }
